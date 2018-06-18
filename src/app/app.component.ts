5a41030e77683c22f8af98aac48bb165bedbc394
JuanYanezTWK <jcarlosyz@gmail.com>
2018-06-18 12:52:02 -0400

    Working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+<<<<<<< origin/service-juancarlos
 
+=======
+import { Http, Response} from '@angular/http';
+import { LlamadoService } from './llamado.service';
+>>>>>>> local
 
 
 @Component({
@@ -8,10 +13,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+<<<<<<< origin/service-juancarlos
 
      constructor() {}
 
      
 
 
+=======
+  title = 'Indicadores económicos en Chile';
+  type = '';
+  date = '';
+  value = '';
+    
+  constructor(private llamadoService: LlamadoService){
+  }
+    
+>>>>>>> local
 }
