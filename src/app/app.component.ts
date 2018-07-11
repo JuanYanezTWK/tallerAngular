604357ad64cb30d69267d71e6cc4f67a058b259b
JuanYanezTWK <jcarlosyz@gmail.com>
2018-07-11 02:05:53 -0400

    change html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,20 +9,30 @@ import { LlamadoService } from './llamado.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Indicadores económicos en Chile';
-  type = '';
-  date = '';
-  value = '';
-  
+  id_select = '';
+  id = '';
+  price = '';
+  description = '';
+  id2 = '';
+  price2 = '';
+  description2 = '';
+
   constructor(private llamadoService: LlamadoService){
-      
-}
-do(){
-this.llamadoService.dateInspect = this.date;
-this.llamadoService.typeInspect = this.type;
-this.value = this.llamadoService.valueUf;
-this.llamadoService.searchIndicator();
-
-}
-
+    
+  }
+  
+  uno() {
+    this.id = this.llamadoService.id;
+    this.price = this.llamadoService.price;
+    this.description = this.llamadoService.description;
+    this.llamadoService.llamarUno();  
+  }
+  todo() {
+    this.llamadoService.id_select = this.id_select;
+    this.id2 = this.llamadoService.id2;
+    this.price2 = this.llamadoService.price2;
+    this.description2 = this.llamadoService.description2;
+    this.llamadoService.llamarTodo();  
+  }
+  
 }
--- src/app/llamado.service.ts
+++ src/app/llamado.service.ts
@@ -6,28 +6,54 @@ import { Http, Response} from '@angular/http';
 })
 export class LlamadoService {
 
-    typeInspect = '';
-    dateInspect = '';
-    valueUf = '';
+  id_select = '';
+  id = '';
+  price = '';
+  description = '';
+  id2 = '';
+  price2 = '';
+  description2 = '';
 
-     constructor(private http: Http) {}
+  constructor(private http: Http) {}
 
-   searchIndicator() {
-       this.http.get('https://mindicador.cl/api/'+this.typeInspect+'/'+this.dateInspect)
-       .subscribe(
-           (res: Response) => {
-             const valueIndicator = res.json();
-             console.log(valueIndicator);
+  llamarTodo() {
+    this.http.get('http://localhost:8080/api/products')
+    .subscribe(
+        (res: Response) => {
+          const datos = res.json();
+          console.log(datos);
             
-             this.valueUf = valueIndicator['serie'][0]['valor'];
-             
-           }, err => {
-             console.log('UPS!');
-             console.log(err);
-           }, () => {
-             console.log('¡Servicio Finalizado!');
-          
-           }
-       );
-     }
+              this.id = datos[0]['id'];
+              this.price = datos[0]['price'];
+              this.description = datos[0]['description'];              
+            
+        }, err => {
+          console.log('UPS!');
+          console.log(err);
+        }, () => {
+          console.log('¡Servicio Finalizado!');
+       
+        }
+    );
+  }
+
+  llamarUno() {
+    this.http.get('http://localhost:8080/api/products/'+ this.id_select)
+    .subscribe(
+        (res: Response) => {
+          const datos2 = res.json();
+          console.log(datos2);
+            this.id2 = datos2['id'];
+            this.price2 = datos2['price'];
+            this.description2 = datos2['description'];
+            
+        }, err => {
+          console.log('UPS!');
+          console.log(err);
+        }, () => {
+          console.log('¡Servicio Finalizado!');
+       
+        }
+    );
+  }
 }
